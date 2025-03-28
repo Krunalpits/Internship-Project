78a3dab10abca841c995b994a9fe7c9fe51e4412
Krunalpits <125281938+Krunalpits@users.noreply.github.com>
2025-03-28 16:25:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ const App = () => {
     return filtered;
   };
 
+// filter user method or function.
   const filteredUsers = getFilteredAndSortedUsers();
 
   // Toggle detailed view for viewimg a user
